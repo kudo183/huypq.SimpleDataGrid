1d68f2d26a7c54581782e587dabc8bdaf15fefe3
kudo183 <kudo183@gmail.com>
2016-09-13 10:20:19 +0700

    - add ReadOnlyGridView.

---
+++ SimpleDataGrid/ReadOnlyGridView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleDataGrid
+{
+    /// <summary>
+    /// Interaction logic for ReadOnlyGridView.xaml
+    /// </summary>
+    public partial class ReadOnlyGridView : UserControl
+    {
+        public List<DataGridColumn> Columns { get; set; }
+        public List<UIElement> CustomMenuItems { get; set; }
+
+        public object SelectedItem { get { return dataGrid.SelectedItem; } }
+
+        private bool _isFirstLoaded = true;
+
+        public ReadOnlyGridView()
+        {
+            Columns = new List<DataGridColumn>();
+            CustomMenuItems = new List<UIElement>();
+            InitializeComponent();
+
+            Loaded += EditableGridView_Loaded;
+
+        }
+
+        void EditableGridView_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitializeWhenFirstLoaded();
+        }
+
+        void InitializeWhenFirstLoaded()
+        {
+            if (_isFirstLoaded == false)
+                return;
+
+            foreach (var dataGridColumn in Columns)
+            {
+                dataGrid.Columns.Add(dataGridColumn);
+            }
+            foreach (var uiElement in CustomMenuItems)
+            {
+                sp.Children.Add(uiElement);
+            }
+
+            _isFirstLoaded = false;
+        }
+    }
+}
