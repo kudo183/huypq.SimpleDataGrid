2506578a040e03208b5400193bd40883624d679c
kudo183 <kudo183@gmail.com>
2016-12-20 19:11:13 +0700

    - remove SelectedItem, only use SelectedValue (ID) because SelectedValue change event fire before SelectedItem, access SelectedItem in SelectedValue change event is wrong because SelectedItem not update yet.

--- SimpleDataGrid/ViewModel/EditableGridViewModel.cs
+++ SimpleDataGrid/ViewModel/EditableGridViewModel.cs
@@ -3,8 +3,7 @@
     public interface IEditableGridViewModel<T>
     {
         ObservableCollectionEx<T> Entities { get; set; }
-        object SelectedValue { get; set; }
-        object SelectedItem { get; set; }        
+        object SelectedValue { get; set; } 
         PagerViewModel PagerViewModel { get; set; }
         SimpleCommand LoadCommand { get; set; }
         SimpleCommand SaveCommand { get; set; }
