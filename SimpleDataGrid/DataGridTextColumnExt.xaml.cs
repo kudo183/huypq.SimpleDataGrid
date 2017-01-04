54709778f2e236f73883b132484ee63a0868453d
kudo183 <kudo183@gmail.com>
2017-01-04 18:34:09 +0700

    - add SetStyle methods, change default HorizonAlign to Center, Stretch when TextFilter

--- SimpleDataGrid/DataGridTextColumnExt.xaml.cs
+++ SimpleDataGrid/DataGridTextColumnExt.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace SimpleDataGrid
 {
@@ -10,6 +11,23 @@ namespace SimpleDataGrid
         public DataGridTextColumnExt()
         {
             InitializeComponent();
+        }
+
+        public void SetStyleAsRightAlign()
+        {
+            var elementStyle = new Style(typeof(TextBlock));
+            elementStyle.Setters.Add(new Setter(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Right));
+            elementStyle.Setters.Add(new Setter(TextBlock.MarginProperty, new Thickness(0, 0, 3, 0)));
+            ElementStyle = elementStyle;
+            var editingElementStyle = new Style(typeof(TextBox));
+            editingElementStyle.Setters.Add(new Setter(TextBox.HorizontalContentAlignmentProperty, HorizontalAlignment.Right));
+            EditingElementStyle = editingElementStyle;
+        }
+
+        public void SetStyleAsRightAlignIntegerNumber()
+        {
+            Binding.StringFormat = "{0:N0}";
+            SetStyleAsRightAlign();
         }
     }
 }
