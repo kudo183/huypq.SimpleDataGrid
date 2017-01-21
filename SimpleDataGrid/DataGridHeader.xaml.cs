397608b69da49cf5ae1e6a2e0c8412ed74aaa954
kudo183 <kudo183@gmail.com>
2017-01-21 16:34:40 +0700

    - add DataGridHeader with content placeholder for the Filter control.

---
+++ SimpleDataGrid/DataGridHeader.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SimpleDataGrid
+{
+    /// <summary>
+    /// Interaction logic for DataGridHeader.xaml
+    /// </summary>
+    public partial class DataGridHeader : UserControl
+    {
+        public DataGridHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
