4804671bd42bcb0a5d07594b8c771e0d76a5acea
kudo183@gmail.com <kudo183@gmail.com>
2017-05-03 11:11:19 +0700

    - add TextManager.

---
+++ SimpleDataGrid/TextManager.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleDataGrid
+{
+    public class TextManager
+    {
+        static TextManager()
+        {
+            page = "page";
+            of = "of";
+            rows = "wows";
+            load = "Load";
+            save = "Save";
+        }
+        
+        private static string page;
+        public static string Page { get { return page; } set { page = value; } }
+
+        private static string of;
+        public static string Of { get { return of; } set { of = value; } }
+
+        private static string rows;
+        public static string Rows { get { return rows; } set { rows = value; } }
+
+        private static string load;
+        public static string Load { get { return load; } set { load = value; } }
+
+        private static string save;
+        public static string Save { get { return save; } set { save = value; } }
+    }
+}
