d1684507e1e6cf267a3da6d4458ced6bbf713492
kudo183@gmail.com <kudo183@gmail.com>
2017-05-17 18:47:20 +0700

    - add DataGridRightAlignTextColumn.

---
+++ SimpleDataGrid/DataGridRightAlignTextColumn.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleDataGrid
+{
+    /// <summary>
+    /// Interaction logic for DataGridRightAlignTextColumn.xaml
+    /// </summary>
+    public partial class DataGridRightAlignTextColumn : DataGridTextColumn
+    {
+        public DataGridRightAlignTextColumn()
+        {
+            InitializeComponent();
+        }
+    }
+}
