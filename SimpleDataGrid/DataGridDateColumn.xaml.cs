caab4500ad210f262f361268cbf0a2e39864e05a
kudo183@gmail.com <kudo183@gmail.com>
2017-05-19 17:47:28 +0700

    - update DataGridDateColumn to commit edit and move to next row when hit Enter (same behavior as Combobox and Text column).

--- SimpleDataGrid/DataGridDateColumn.xaml.cs
+++ SimpleDataGrid/DataGridDateColumn.xaml.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 
 namespace SimpleDataGrid
 {
@@ -24,12 +25,31 @@ namespace SimpleDataGrid
                 dp.SelectedDate = DateTime.Parse(uneditedValue.ToString());
             }
         }
+
         protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
         {
             var dp = new DatePicker { };
             dp.SetBinding(DatePicker.SelectedDateProperty, Binding);
+            
+            dp.PreviewKeyDown += dp_PreviewKeyDown;
 
             return dp;
+        }
+
+        private void dp_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            //because the DatePicker textbox has set e.Handled=true when Enter keydown event,
+            //show need manully raise the Enter keydown event for DataGrid to commit current edit and move to next row
+            if (e.Key == Key.Enter)
+            {
+                var evt = new KeyEventArgs(Keyboard.PrimaryDevice, PresentationSource.FromVisual(this.DataGridOwner), 0, e.Key)
+                {
+                    RoutedEvent = Keyboard.KeyDownEvent
+                };
+                
+                //don't know why the DataGrid will get one more event from DataGridCell (2 event fired, can check by override DataGrid OnKeyDown method)
+                this.DataGridOwner.RaiseEvent(evt);
+            }
         }
 
         protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
