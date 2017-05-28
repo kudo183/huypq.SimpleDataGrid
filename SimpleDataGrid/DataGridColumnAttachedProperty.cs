3ebd65b437cc278dbe527996aa0ee117ac0a0d1c
kudo183@gmail.com <kudo183@gmail.com>
2017-05-28 15:20:27 +0700

    - correct typename.

--- SimpleDataGrid/DataGridColumnAttachedProperty.cs
+++ SimpleDataGrid/DataGridColumnAttachedProperty.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace SimpleDataGrid
 {
@@ -22,6 +16,6 @@ namespace SimpleDataGrid
 
         // Using a DependencyProperty as the backing store for IsTabStop.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IsTabStopProperty =
-            DependencyProperty.RegisterAttached("IsTabStop", typeof(bool), typeof(DataGridColumn), new PropertyMetadata(true));
+            DependencyProperty.RegisterAttached("IsTabStop", typeof(bool), typeof(DataGridColumnAttachedProperty), new PropertyMetadata(true));
     }
 }
