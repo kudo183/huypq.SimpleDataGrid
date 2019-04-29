bea403ecd601aa18b3ee824cde80803c6abb9da7
win10 desktop <kudo183@gmail.com>
2019-04-30 03:30:51 +0700

    - add DataGridByteArrayColumn.

---
+++ SimpleDataGrid/Converter/ByteArrayToBase64TextConverter.cs
@@ -0,0 +1,21 @@
+﻿using SimpleDataGrid.ViewModel;
+using System;
+using System.Windows.Data;
+
+namespace SimpleDataGrid.Converter
+{
+    public class ByteArrayToBase64TextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var byteArray = (byte[])value;
+            return System.Convert.ToBase64String(byteArray);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var base64String = (string)value;
+            return System.Convert.FromBase64String(base64String);
+        }
+    }
+}
---
+++ SimpleDataGrid/DataGridByteArrayColumn.xaml.cs
@@ -0,0 +1,42 @@
+﻿using SimpleDataGrid.Converter;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SimpleDataGrid
+{
+    /// <summary>
+    /// Interaction logic for DataGridByteArrayColumn.xaml
+    /// </summary>
+    public partial class DataGridByteArrayColumn : DataGridTextColumn
+    {
+        static ByteArrayToBase64TextConverter _converter = new ByteArrayToBase64TextConverter();
+        public DataGridByteArrayColumn()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBindingChanged(BindingBase oldBinding, BindingBase newBinding)
+        {
+            base.OnBindingChanged(oldBinding, newBinding);
+            (newBinding as System.Windows.Data.Binding).Converter = _converter;
+        }
+
+        public void SetStyleAsRightAlign()
+        {
+            var elementStyle = new Style(typeof(TextBlock));
+            elementStyle.Setters.Add(new Setter(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Right));
+            elementStyle.Setters.Add(new Setter(TextBlock.MarginProperty, new Thickness(0, 0, 3, 0)));
+            ElementStyle = elementStyle;
+            var editingElementStyle = new Style(typeof(TextBox));
+            editingElementStyle.Setters.Add(new Setter(TextBox.HorizontalContentAlignmentProperty, HorizontalAlignment.Right));
+            EditingElementStyle = editingElementStyle;
+        }
+
+        public void SetStyleAsRightAlignIntegerNumber()
+        {
+            Binding.StringFormat = "{0:N0}";
+            SetStyleAsRightAlign();
+        }
+    }
+}
